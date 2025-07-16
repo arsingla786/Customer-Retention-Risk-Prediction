0c1f897f6e57652dfbcfeaa9527a677a71df8992
arnav <arnavsingla06@gmail.com>
2025-07-16 19:02:57 +0530

    added random forest model

--- CustomerRetentionRiskPrediction.py
+++ CustomerRetentionRiskPrediction.py
@@ -21,6 +21,7 @@ By predicting customer churn, the company can proactively design retention strat
 keep these customers, thereby improving customer satisfaction and reducing financial loss.
 '''
 import pandas as pd
+print(1)
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -33,12 +34,13 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 #load the dataset using pandas 
 df = pd.read_csv("C:\\Users\\Arnav Singla\\Downloads\\Customer_data.csv")
-
+#drop null values
+df = df.dropna()
 #print some rows of data
 print(df.head())
 
 #Through graphs, analyse the data
-
+'''
 plt.figure(figsize=(5,7))
 sns.countplot(data=df , x='gender')
 plt.xlabel('Gender')
@@ -66,12 +68,12 @@ plt.show()
 plt.figure(figsize=(10, 6))
 sns.heatmap(df.corr(numeric_only=True), annot=True, cmap='coolwarm')
 plt.title('Correlation Heatmap')
-plt.show()
+plt.show()'''
 
 #Encode the data which is non numeric like gender , Payment methods, subscription status4
 
 label_encoders = {}
-for column in ['gender','Partner','Dependents','PhoneService','MultipleLines',
+for column in ['customerID','gender','Partner','Dependents','PhoneService','MultipleLines',
                'InternetService','OnlineSecurity','OnlineBackup','DeviceProtection',
                'TechSupport','StreamingTV','StreamingMovies','Contract','PaperlessBilling',
                'PaymentMethod','Churn']:
@@ -85,31 +87,43 @@ print(df.head())
 #define features(factors used to predict) and target(what to predict)
 
 #features
-X = df.drop(['gender','Partner','Dependents','PhoneService','MultipleLines',
-               'InternetService','OnlineSecurity','OnlineBackup','DeviceProtection',
-               'TechSupport','StreamingTV','StreamingMovies','Contract','PaperlessBilling',
-               'PaymentMethod'],axis=1) 
+X = df.drop('Churn',axis=1) 
 
 #target
 y= df['Churn']
 
 #Split into test and training data
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=0)
 
 # Scaling - to  maintain the range like [0-1] or [0-1000] of each data features
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-#Logistic regression model 
-logreg = LogisticRegression()
+#______________Logistic regression model_____________
+logreg = LogisticRegression(class_weight='balanced')
 logreg.fit(X_train,y_train)
 logreg_pred = logreg.predict(X_test)
 logreg_accuracy = accuracy_score(y_test,logreg_pred)
 
 #display the results 
+print('FOR LOGISTIC REGRESSION MODEL : ')
 print(f'Accuracy: {logreg_accuracy * 100:.2f}%')
 print("\nClassification Report:")
 print(classification_report(y_test, logreg_pred))
+
+#______________RANDOM FOREST MODEL___________________
+from sklearn.ensemble import RandomForestClassifier
+
+rf_model = RandomForestClassifier(class_weight='balanced', random_state=42)
+rf_model.fit(X_train,y_train)
+rf_pred = rf_model.predict(X_test)
+rf_accuracy = accuracy_score(y_test,rf_pred)
+
+#display the results 
+print('FOR RANDOM FOREST MODEL : ')
+print(f'Accuracy: {rf_accuracy * 100:.2f}%')
+print("\nClassification report :")
+print(classification_report(y_test,rf_pred))
 
