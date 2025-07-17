8f37c1b17f692516a36cf01dd5699f1423f8a3c5
Arnav <arnavsingla06@gmail.com>
2025-07-18 02:12:24 +0530

    Update and rename CustomerRetentionRiskPrediction.py to Customer_Loyalty_Prediction.py

--- Customer_Loyalty_Prediction.py
+++ Customer_Loyalty_Prediction.py
@@ -6,20 +6,8 @@ Predicting customer churn, can help organisations
 take proactive steps to retain customers, thus minimizing revenue loss.
 This project aims to build a machine learning model that can predict
 whether a customer will churn based on their demographic, account,
-and service-related data.
+and service-related data.'''
 
-2. Problem Statement
-The goal of this project is to develop a classification model that predicts whether 
-customer
-will churn. Using demographic data (such as gender, senior citizen status, and tenure),
-along
-with information about the services they use (such as internet service, phone service, and
-online security), we will attempt to build a model that helps the company identify
-customers who are at a high risk of churning.
-
-By predicting customer churn, the company can proactively design retention strategies to
-keep these customers, thereby improving customer satisfaction and reducing financial loss.
-'''
 import pandas as pd
 import numpy as np
 import seaborn as sns
